efba4b12448206358517001eba0df1cabb0c9694
jkjeong <wjdrnr6176@gmail.com>
2020-02-09 22:52:55 +0900

    done.

---
+++ Book/Python Studying Alone/Mission/200209.py
@@ -0,0 +1,28 @@
+str_input = input("태어난 해를 입력해주세요> ")
+birth_input = int(str_input) % 12
+
+if birth_input == 0 :
+    print("원숭이 띠입니다.")
+elif birth_input == 1:
+    print("닭 띠입니다.")
+elif birth_input == 2:
+    print("개 띠입니다.")
+elif birth_input == 3:
+    print("돼지 띠입니다.")
+elif birth_input == 4:
+    print("쥐 띠입니다.")
+elif birth_input == 5:
+    print("소 띠입니다.")
+elif birth_input == 6:
+    print("범 띠입니다.")
+elif birth_input == 7:
+    print("토끼 띠입니다.")
+elif birth_input == 8:
+    print("용 띠입니다.")
+elif birth_input == 9:
+    print("뱀 띠입니다.")
+elif birth_input == 10:
+    print("말 띠입니다.")
+elif birth_input == 11:
+    print("양 띠입니다.")
+
